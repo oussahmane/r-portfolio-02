ef71223b5b7dda9bd048f90e35c4ee5a69a101d8
oussahmane <oussahmane@gmail.com>
2026-04-18 22:55:03 +0200

    fixing github pages deploy issue

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/rt-portfolio-01/',
+  base: '/r-portfolio-02/',
 })
